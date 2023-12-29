ba72063b995157106f18aaa6126935d6a79ce6d7
Scrpfr <143892422+Scrpfr@users.noreply.github.com>
2023-12-30 01:23:36 +0500

    Update marie.js

--- characters/marie.js
+++ characters/marie.js
@@ -5,28 +5,28 @@ const marie = {
    p_active: '14% за каждый истёкший бафф или дебафф любого перса.'
 }
 
-function denile() {
-   person.card_name = 'Нилисток';
-   person.aka = 'Нилисток';
-   person.card_src = 'In_Denile.png';
-   person.description = 'Очень слабая карточка, не рекомендуется прокачивать. Находит своё место только на аватарке в виде алмазной шайни из-за своей красоты.';
-   person.ability = 'При переходе в Сехмет снимает все отрицательные эффекты и дает иммунитет на 15 сек, а врагу раскол брони.';
+function maid_of_honor() {
+   person.card_name = 'Рыцарь-горничная';
+   person.aka = 'Рыцарь';
+   person.card_src = 'Maid_of_Honor.png';
+   person.description = 'Персонаж чисто саппорт, ей не нужно самостоятельно сражаться. Очень полезная и доступная, из-за серебрянного ранга, отлично подходит против нечисти и жажды смерти. А с Возрождением и персоной в команде, можно задефать основного атакующего персонажа, тоже полезно.';
+   person.ability = 'Пока Мари жива, блокирование атак любым из персонажей с шансом 50% превращает 1 дебафф в барьер. Активный персонаж с барьером восстанавливает 2% здоровья в секунду.';
    person.marquee1 = `${marie.marquee1}`;
-   person.marquee2 = `${marie.marquee2}`;
+   person.marquee2 = `${marie.marquee2} Лучший выбор.`;
    person.prestige_description = `${marie.prestige}`;
    person.prestige_activation = `${marie.p_active}`;
-   person.at = '7,392';
-   person.hp = '50,381';
-   person.atk_stats = ['Атака', 'Проницание', 'Шанс крита', 'Урон крита'];
-   person.def_stats = ['Не защитник'];
-   person.build_1_img = 'eliza/Sekhmets_Bite.png';
-   person.build_2_img = 'eliza/Sekhmets_Fury.png';
-   person.build_3_img = 'eliza/Sekhmets_Turn.png';
-   person.build_4_img = 'eliza/Chaos_Banish.png';
-   person.build_1_desc = 'Сехмет с шансом дать проклятье.';
-   person.build_2_desc = 'Сехмет с шансом получить бешенство.';
-   person.build_3_desc = 'Сехмет с шансом дать кровотечение и блок исцеления..';
-   person.build_4_desc = 'Снимает все положительные эффекты врага.';
+   person.at = '6,471';
+   person.hp = '64,704';
+   person.atk_stats = ['Только саппорт'];
+   person.def_stats = ['Только саппорт'];
+   person.build_1_img = 'marie/Weight_of_our_Will.png';
+   person.build_2_img = 'marie/Scrub_n_Smash.png';
+   person.build_3_img = 'marie/Suction_Obstruction.png';
+   person.build_4_img = 'marie/Marie_Burst.png';
+   person.build_1_desc = 'Каждый удар снимает 2% шкалы блокбастера врага.';
+   person.build_2_desc = 'Шанс снять все баффы противника.';
+   person.build_3_desc = 'Неблокируемый захват.';
+   person.build_4_desc = 'Сбивает атаку противника.';
    skullgirls.replaceChildren();
    pageGenerator();
 }
