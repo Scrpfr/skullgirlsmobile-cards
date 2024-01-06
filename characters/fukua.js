a50a2f17472b2e5c5b56c1ad01e1078eef952dac
Scrpfr <143892422+Scrpfr@users.noreply.github.com>
2024-01-06 23:39:17 +0500

    Update fukua.js

--- characters/fukua.js
+++ characters/fukua.js
@@ -1,7 +1,7 @@
 const fukua = {
    marquee1: 'Ярость - доп 1% урона за каждые 5% потерянного здоровья.',
    marquee2: 'Гнев - снижает получаемый урон на 1% за каждые потерянные 5% здоровья.',
-   prestige: 'Забирает у врага 5% шкалы бб и даёт ему блок исцеления при исп блокбастера.',
+   prestige: 'При активации блокбастера, забирает ~5% шкалы и даёт блок исцеления.',
    p_active: '25% за использование блокбастера.'
 }
 
@@ -52,6 +52,82 @@ function infernal_twin() {
    person.build_2_desc = 'Используется в комбинации.';
    person.build_3_desc = 'Может передать дебаффы и забрать баффы.';
    person.build_4_desc = 'Может дать метку смерти, раскол брони и увечье.';
+   skullgirls.replaceChildren();
+   pageGenerator();
+}
+
+function souls_sister() {
+   person.card_name = 'Сестра по духу';
+   person.aka = 'Ермак';
+   person.card_src = 'Souls_Sister.png';
+   person.description = 'Весьма хорошая вариация, достойная прокачки, но может быстро надоесть, так как в текущей игре зонеры очень ограничены в возможностях.';
+   person.ability = 'Если противник далеко, заряжает шкалу блокбастера на 3% в секунду и даёт бешенство на 10 сек при ударе.';
+   person.marquee1 = `${fukua.marquee1} Только ярость.`;
+   person.marquee2 = `${fukua.marquee2}`;
+   person.prestige_description = `${fukua.prestige}`;
+   person.prestige_activation = `${fukua.p_active}`;
+   person.at = '9,946';
+   person.hp = '41,127';
+   person.atk_stats = ['Атака', 'Проницание', 'Шанс крита', 'Урон крита'];
+   person.def_stats = ['Не защитник'];
+   person.build_1_img = 'fukua/Love_Dart.png';
+   person.build_2_img = 'fukua/Lobs_of_Love.png';
+   person.build_3_img = 'fukua/Best_Friends_Forever.png';
+   person.build_4_img = 'fukua/Goodnight_Kiss.png';
+   person.build_1_desc = 'Низкий урон, но дистанционный.';
+   person.build_2_desc = 'Для дальней дистанции.';
+   person.build_3_desc = 'Может передать дебаффы и забрать баффы.';
+   person.build_4_desc = 'Шанс дать иссушение и усталость.';
+   skullgirls.replaceChildren();
+   pageGenerator();
+}
+function rain_shadow() {
+   person.card_name = 'Грозовая тень';
+   person.aka = 'Рэйн';
+   person.card_src = 'Rain_Shadow.png';
+   person.description = 'Потрясающий антибаффер, и оглушает случайно. Очень сильный серебрянный персонаж, можно качать и алмазить: в алмазном ранге также может пригодиться.';
+   person.ability = 'При ударах в блок шанс 15% дать раскол брони и всплеск энергии на 15 сек. Если противник стоит на вашей стороне и не оглушён, через каждые 5 сек враг теряет все баффы и получает усталость на 10 сек.';
+   person.marquee1 = `${fukua.marquee1} Только ярость.`;
+   person.marquee2 = `${fukua.marquee2}`;
+   person.prestige_description = `${fukua.prestige}`;
+   person.prestige_activation = `${fukua.p_active}`;
+   person.at = '8,640';
+   person.hp = '45,716';
+   person.atk_stats = ['Атака', 'Проницание', 'Шанс крита', 'Урон крита'];
+   person.def_stats = ['Не защитник'];
+   person.build_1_img = 'fukua/Drill_of_My_Dreams.png';
+   person.build_2_img = 'fukua/Breakdown.png';
+   person.build_3_img = 'fukua/Forever_a_Clone.png';
+   person.build_4_img = 'fukua/Fukua_Drillationship.png';
+   person.build_1_desc = 'Используется в комбинации.';
+   person.build_2_desc = 'Получить миазмы.';
+   person.build_3_desc = 'Наложит блок исцеления.';
+   person.build_4_desc = 'Может дать метку смерти, раскол брони и увечье.';
+   skullgirls.replaceChildren();
+   pageGenerator();
+}
+function chameleon_twist() {
+   person.card_name = 'Ящерица';
+   person.aka = 'Хамелеон';
+   person.card_src = 'Chameleon_Twist.png';
+   person.description = 'Неплохой универсал, в атаке можно давить миазмами, в защите опасна уклонением. Как серебрянный и золотой танк сойдет, алмазить не стоит.';
+   person.ability = 'Удар по движущемуся противнику даст миазмы на 10 сек. При ударе пока не заряжен блокбастер шанс 5% получить уклонение и миазмы на 10 сек.';
+   person.marquee1 = `${fukua.marquee1}`;
+   person.marquee2 = `${fukua.marquee2}`;
+   person.prestige_description = `${fukua.prestige}`;
+   person.prestige_activation = `${fukua.p_active}`;
+   person.at = '7,335';
+   person.hp = '50,324';
+   person.atk_stats = ['Атака', 'Проницание', 'Шанс крита', 'Урон крита'];
+   person.def_stats = ['Здоровье', 'Защита', 'Сопротивление', 'Сопрот криту'];
+   person.build_1_img = 'fukua/Breakdown.png';
+   person.build_2_img = 'fukua/Drill_of_My_Dreams.png';
+   person.build_3_img = 'fukua/Best_Friends_Forever.png';
+   person.build_4_img = 'fukua/Goodnight_Kiss.png';
+   person.build_1_desc = 'Получить миазмы.';
+   person.build_2_desc = 'Больше урона в комбо.';
+   person.build_3_desc = 'Может передать дебаффы и забрать баффы.';
+   person.build_4_desc = 'Шанс дать иссушение и усталость.';
    skullgirls.replaceChildren();
    pageGenerator();
 }
